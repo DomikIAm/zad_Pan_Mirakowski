ad6a1e3ecb815087d35d6431f1493eafa76ab385
JD <57227707+DomikIAm@users.noreply.github.com>
2022-01-19 12:51:39 +0100

    Create jd.py

---
+++ jd.py
@@ -0,0 +1,22 @@
+
+# ZAD 1
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+i = int(input())
+tab = [a,b,c], [d,e,f], [g,h,i]
+print(min(tab))
+print(max(tab))
+
+
+#ZAD 2
+"""
+tab = [1,2,3],[4,5,6],[7,8,9]
+print(min(tab[1]))
+print(max(tab[1]))
+"""
